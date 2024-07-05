cb1db45e05903f8a05c09ad919f90e2a699a3fa9
arwindersngh62 <arwindersngh62@gmail.com>
2024-07-05 22:23:49 +0200

    Formatted code and then cleaned switchboard

--- pyenigma_api/__init__.py
+++ pyenigma_api/__init__.py
@@ -2,5 +2,5 @@ from main import EnigmaInterface
 
 if __name__ == "__main__":
     __version__ = "0.0.3"
-    __author__ = 'Arwinder Singh'
-    __credits__ = 'Jared Owen'
+    __author__ = "Arwinder Singh"
+    __credits__ = "Jared Owen"
